3eb80812d9f25ee4ec24e18ef2241c9bcc4aa822
vinishadevu <vinishadevu888@gmail.com>
2023-04-25 17:57:17 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -12,4 +12,15 @@ def check_for_len(value):
 class StudentForm(forms.Form):
     name=forms.CharField(max_length=100,validators=[check_for_a,check_for_len])
     age=forms.IntegerField()
-    email=forms.EmailField(validators=[check_for_a])
+    email=forms.EmailField()
+    re_enter_email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+    def clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('not matched')
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot')
